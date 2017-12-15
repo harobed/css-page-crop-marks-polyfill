132d0542d37740572adef47857cd0364707ca9e9
Stéphane Klein <contact@stephane-klein.info>
2017-12-15 15:39:19 +0100

    Add page size and margin support + add README

---
+++ README.md
@@ -0,0 +1,8 @@
+Enable [css-page-cross-marks-polyfill.js](css-page-cross-marks-polyfill.js) in your html page:
+
+```
+<head>
+  ...
+  <script data-page-width="210mm" data-page-height="297mm" data-page-margin="10mm" src="https://cdn.rawgit.com/harobed/css-page-crop-marks-polyfill/master/css-page-cross-marks-polyfill.js"></script>
+</head>
+```
--- css-page-cross-marks-polyfill.js
+++ css-page-cross-marks-polyfill.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+  function getParam(name) {
+    elements = document.querySelector("*[data-" + name + "]");
+    if (elements) {
+      return elements.attributes['data-' + name].nodeValue;
+    }
+  }
+  page_margin = getParam('page-margin') ? getParam('page-margin') : '10mm';
+  page_height = getParam('page-height') ? getParam('page-height') : '297mm';
+  page_width = getParam('page-width') ? getParam('page-width') : '210mm';
+
   var wrapper = document.createElement('div');
   wrapper.style.cssText = "position: absolute; top: 0; left: 0; z-index: 1000;"
   document.body.appendChild(wrapper);
   for(var i=0; i < 10; i++) {
     var page = document.createElement('div');
-    page.style.cssText = "position: absolute; top: "+ (i * 297) +"mm; left: 0; border: 1px dashed #333; height: 297mm; width: 210mm;"
+    page.style.cssText = "position: absolute; top: calc("+ i + " * (" + page_height + " - (" + page_margin + " * 2))); left: 0; border: 1px dashed #333; height: calc(" + page_height + " - (" + page_margin + " * 2)); width: calc(" + page_width + " - (" + page_margin + " * 2));"
     wrapper.appendChild(page);
   }
 }, false);
